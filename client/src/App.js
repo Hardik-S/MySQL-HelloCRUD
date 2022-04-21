95484d4967271d4c7616efa3d798872f388088bb
Hardik Shrestha <hardik.ho@gmail.com>
2022-04-20 22:52:38 -0400

    repo name

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ function App() {
       (response) => {
         setEmployeeList(
           employeeList.map((val) => {
-            return val.id == id
+            return val.id === id
               ? {
                   id: val.id,
                   name: val.name,
@@ -65,7 +65,7 @@ function App() {
     Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
       setEmployeeList(
         employeeList.filter((val) => {
-          return val.id != id;
+          return val.id !== id;
         })
       );
     });
@@ -76,7 +76,9 @@ function App() {
       <div className="information">
         <label>Name:</label>
         <input
-          type="text"
+          type="text" 
+          onkeyup="if(this.value<0){this.value= this.value * -1}"
+          min="15"
           onChange={(event) => {
             setName(event.target.value);
           }}
