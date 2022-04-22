7af1b0de9f1c4574ef361d452e3d5872b486a1c4
Hardik Shrestha <hardik.ho@gmail.com>
2022-04-22 09:20:02 -0400

    cors and axios use

--- client/src/App.js
+++ client/src/App.js
@@ -1,198 +1,213 @@
-import "./App.css";
-import { useState } from "react";
-import Axios from "axios";
+// import dependencies including axios for API requests
+import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+    // simple state variables and hooks
+    const [name, setName] = useState('Rythm');
+    const [age, setAge] = useState(18);
+    const [country, setCountry] = useState('Canada');
+    const [position, setPosition] = useState('CEO');
+    const [wage, setWage] = useState(0);
+    const [startdate, setstartdate] = useState('01/01/1970');
 
-  // simple state variables and hooks
-  const [name, setName] = useState("Rythm");
-  const [age, setAge] = useState(18);
-  const [country, setCountry] = useState("Canada");
-  const [position, setPosition] = useState("CEO");
-  const [wage, setWage] = useState(0);
-  const [startdate, setstartdate] = useState("01/01/1970");
+    // complex state hooks
+    const [newWage, setNewWage] = useState(0);
+    const [employeeList, setEmployeeList] = useState([]);
 
-  // complex state hooks
-  const [newWage, setNewWage] = useState(0);
-  const [employeeList, setEmployeeList] = useState([]);
-
-  // addEmployee function
-  const addEmployee = () => {
-    Axios.post("http://localhost:3001/create", {
-      name: name,
-      age: age,
-      country: country,
-      position: position,
-      wage: wage,
-      startdate: startdate,
-    }).then(() => {
-      setEmployeeList([
-        ...employeeList,
-        {
-          name: name,
-          age: age,
-          country: country,
-          position: position,
-          wage: wage,
-          startdate: startdate,
-        },
-      ]);
-    });
-  };
-
-  // getEmployees function
-  const getEmployees = () => {
-    Axios.get("http://localhost:3001/employees").then((response) => {
-      setEmployeeList(response.data);
-    });
-  };
-
-  const updateEmployeeWage = (id) => {
-    Axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
-      (response) => {
-        setEmployeeList(
-          employeeList.map((val) => {
-            return val.id === id
-              ? {
-                  id: val.id,
-                  name: val.name,
-                  country: val.country,
-                  age: val.age,
-                  position: val.position,
-                  wage: newWage,
-                  startdate: val.startdate,
+    // addEmployee function
+    const addEmployee = () => {
+	// create object using post request
+        Axios.post('http://localhost:3001/create', {
+            // object properties, assign keys: values
+            name: name,
+            age: age,
+            country: country,
+            position: position,
+            wage: wage,
+            startdate: startdate,
+        }).then(() => {
+	// force synchronous function such that data exists in db before manipulation
+            setEmployeeList([
+                ...employeeList,
+                {
+                    name: name,
+                    age: age,
+                    country: country,
+                    position: position,
+                    wage: wage,
+                    startdate: startdate,
                 }
-              : val;
-          })
+            ]);
+        });
+    };
+
+    // getEmployees function
+    const getEmployees = () => {
+        Axios.get('http://localhost:3001/employees').then((response) => {
+            setEmployeeList(response.data);
+        });
+    };
+
+    const updateEmployeeWage = (id) => {
+        Axios.put('http://localhost:3001/update', {
+            wage: newWage,
+            id: id
+        }).then((response) => {
+            setEmployeeList(
+                employeeList.map((val) => {
+                    return val.id === id
+                        ? {
+                              id: val.id,
+                              name: val.name,
+                              country: val.country,
+                              age: val.age,
+                              position: val.position,
+                              wage: newWage,
+                              startdate: val.startdate
+                          }
+                        : val;
+                })
+            );
+        });
+    };
+
+    const deleteEmployee = (id) => {
+        Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+            setEmployeeList(
+                employeeList.filter((val) => {
+                    return val.id !== id;
+                })
+            );
+        });
+    };
+
+    const displayInfo = () => {
+        console.log(
+            'name: ' +
+                name +
+                '\nage: ' +
+                age +
+                '\ncountry: ' +
+                country +
+                '\nposition: ' +
+                position +
+                '\nwage: ' +
+                wage +
+                '\nstartdate: ' +
+                startdate
         );
-      }
-    );
-  };
+    };
 
-  const deleteEmployee = (id) => {
-    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
-      setEmployeeList(
-        employeeList.filter((val) => {
-          return val.id !== id;
-        })
-      );
-    });
-  };
-
-  const displayInfo = () => {
-    console.log("name: " + name + "\nage: " + age + "\ncountry: " + country + "\nposition: " + position + "\nwage: " + wage + "\nstartdate: " + startdate);
-  };
-
-  return (
-    <div className="App" class = "fullscreen">
-      <div className="information">
-
-        <label>Name:</label>
-        <input
-          type="text" 
-          onKeyUp="if(this.value<0){this.value= this.value * -1}"
-          min="15"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-
-        <label>Age:</label>
-        <input
-          type="number"
-          onChange={(event) => {
-            setAge(event.target.value);
-          }}
-        />
-
-        <label>Country:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setCountry(event.target.value);
-          }}
-        />
-
-        <label>Position:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setPosition(event.target.value);
-          }}
-        />
-
-        <label>Wage (year):</label>
-        <input
-          type="number"
-          min="0"
-          onChange={(event) => {
-            setWage(event.target.value);
-          }}
-        />
-        <label>Startdate:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setstartdate(event.target.value);
-          }}
-        />    
-
-        <button class = "button" 
-        onClick={addEmployee}>
-          <span>Add Employee       </span></button>
-      
-      </div>
-
-      <div className="employees">
-      <button class = "button" 
-        onClick={displayInfo}>
-          <span>Console Log Display Info       </span></button>
-        <button class = "button" 
-        onClick={getEmployees}>
-          <span>Show Employees       </span></button>
-
-        {employeeList.map((val, key) => {
-          return (
-            <div className="employee">
-              <div>
-                <h3>Name: {val.name}</h3>
-                <h3>Age: {val.age}</h3>
-                <h3>Country: {val.country}</h3>
-                <h3>Position: {val.position}</h3>
-                <h3>Wage: {val.wage}</h3>
-                <h3>startdate: {val.startdate}</h3>
-              </div>
-              <div>
+    return (
+        <div className="App" class="fullscreen">
+            <div className="information">
+                <label>Name:</label>
                 <input
-                  type="text"
-                  placeholder="2000..."
-                  onChange={(event) => {
-                    setNewWage(event.target.value);
-                  }}
+                    type="text"
+                    onKeyUp="if(this.value<0){this.value= this.value * -1}"
+                    min="15"
+                    onChange={(event) => {
+                        setName(event.target.value);
+                    }}
                 />
-                <button
-                  onClick={() => {
-                    updateEmployeeWage(val.id);
-                  }}
-                >
-                  {" "}
-                  Update
+
+                <label>Age:</label>
+                <input
+                    type="number"
+                    onChange={(event) => {
+                        setAge(event.target.value);
+                    }}
+                />
+
+                <label>Country:</label>
+                <input
+                    type="text"
+                    onChange={(event) => {
+                        setCountry(event.target.value);
+                    }}
+                />
+
+                <label>Position:</label>
+                <input
+                    type="text"
+                    onChange={(event) => {
+                        setPosition(event.target.value);
+                    }}
+                />
+
+                <label>Wage (year):</label>
+                <input
+                    type="number"
+                    min="0"
+                    onChange={(event) => {
+                        setWage(event.target.value);
+                    }}
+                />
+                <label>Startdate:</label>
+                <input
+                    type="text"
+                    onChange={(event) => {
+                        setstartdate(event.target.value);
+                    }}
+                />
+
+                <button class="button" onClick={addEmployee}>
+                    <span>Add Employee </span>
+                </button>
+            </div>
+
+            <div className="employees">
+                <button class="button" onClick={displayInfo}>
+                    <span>Console Log Display Info </span>
+                </button>
+                <button class="button" onClick={getEmployees}>
+                    <span>Show Employees </span>
                 </button>
 
-                <button
-                  onClick={() => {
-                    deleteEmployee(val.id);
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
+                {employeeList.map((val, key) => {
+                    return (
+                        <div className="employee">
+                            <div>
+                                <h3>Name: {val.name}</h3>
+                                <h3>Age: {val.age}</h3>
+                                <h3>Country: {val.country}</h3>
+                                <h3>Position: {val.position}</h3>
+                                <h3>Wage: {val.wage}</h3>
+                                <h3>startdate: {val.startdate}</h3>
+                            </div>
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder="2000..."
+                                    onChange={(event) => {
+                                        setNewWage(event.target.value);
+                                    }}
+                                />
+                                <button
+                                    onClick={() => {
+                                        updateEmployeeWage(val.id);
+                                    }}
+                                >
+                                    {' '}
+                                    Update
+                                </button>
+
+                                <button
+                                    onClick={() => {
+                                        deleteEmployee(val.id);
+                                    }}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -3,87 +3,88 @@
  */
 
 // import dependencies
-const express = require("express");
+const express = require('express');
 const app = express();
-const mysql = require("mysql");
-const cors = require("cors");
+const mysql = require('mysql');
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
 // mySQL database connection
 const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "password",
-  database: "employeeSystem",     // the created database following camelCase
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'employeeSystem' // the created database following camelCase
 });
 
+// API requests using axios
 // post request to insert (create) employees in database with request and response
-app.post("/create", (req, res) => {
-  // request input data from front end (app.js), if empty then use default values
-  const name = req.body.name;
-  const age = req.body.age;
-  const country = req.body.country;
-  const position = req.body.position;
-  const wage = req.body.wage;
-  const startdate = req.body.startdate;
+app.post('/create', (req, res) => {
+    // request input data from front end (app.js), if empty then use default values
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+    const startdate = req.body.startdate;
 
-  // query using SQL insert statement
-  db.query(
-    "INSERT INTO employees (name, age, country, position, wage, startdate) VALUES (?,?,?,?,?,?)",
-    [name, age, country, position, wage, startdate],
-    (err, result) => {
-      // if there is an error, log it to the console 
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Values successfully inserted...");
-      }
-    }
-  );
+    // query using SQL insert statement
+    db.query(
+        'INSERT INTO employees (name, age, country, position, wage, startdate) VALUES (?,?,?,?,?,?)',
+        [name, age, country, position, wage, startdate],
+        (err, result) => {
+            // if there is an error, log it to the console
+            if (err) {
+                console.log(err);
+            } else {
+                res.send('Values successfully inserted...');
+            }
+        }
+    );
 });
 
-app.get("/employees", (req, res) => {
-  db.query("SELECT * FROM employees", (err, result) => {
-    // if there is an error, log it to the console 
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
+app.get('/employees', (req, res) => {
+    db.query('SELECT * FROM employees', (err, result) => {
+        // if there is an error, log it to the console
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
-app.put("/update", (req, res) => {
-  const id = req.body.id;
-  const wage = req.body.wage;
-  db.query(
-    "UPDATE employees SET wage = ? WHERE id = ?",
-    [wage, id],
-    (err, result) => {
-    // if there is an error, log it to the console 
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query(
+        'UPDATE employees SET wage = ? WHERE id = ?',
+        [wage, id],
+        (err, result) => {
+            // if there is an error, log it to the console
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
 });
 
-app.delete("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('DELETE FROM employees WHERE id = ?', id, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 // Server life check
 app.listen(3001, () => {
-  console.log("Excellent, the server is alive and running! Port: 3001");
+    console.log('Excellent, the server is alive and running! Port: 3001');
 });
